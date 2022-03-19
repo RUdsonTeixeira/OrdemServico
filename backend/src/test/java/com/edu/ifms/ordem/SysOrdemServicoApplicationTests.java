5a0cd73fd9116965b9229c5aff6b0e86056bf564
Rudson <rudson.teixeira@estudante.ifms.edu.br>
2022-03-18 22:00:59 -0300

    firstcommit

---
+++ backend/src/test/java/com/edu/ifms/ordem/SysOrdemServicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.ifms.ordem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysOrdemServicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
