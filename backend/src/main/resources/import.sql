1bf1a49c1f2bafdc3cb348d18d3afdb22d0fffe5
Rudson <rudson.teixeira@estudante.ifms.edu.br>
2022-03-24 22:43:11 -0300

    Seeding no Banco de Dados

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO TB_TECNICO(nome, telefone, email, senha) VALUES ('Rudson Teixeira', '67999997070', 'rudson@gmail.com', '1234');
+INSERT INTO TB_TECNICO(nome, telefone, email, senha)VALUES ('Jorlando Rodrigues', '67999996060', 'jorlando@gmail.com', '1234');
+INSERT INTO TB_TECNICO(nome, telefone, email, senha)VALUES ('Roberta Teixeira', '67999998080', 'roberta@gmail.com', '1234');
